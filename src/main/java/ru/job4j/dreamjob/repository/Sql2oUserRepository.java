7a279f231993c011b96dfd7881c00337be01b384
Pavel Valger <pavelwalker@mail.ru>
2023-07-01 14:17:18 +0500

    4. Многопоточность в базе данных [#504860  [#504860 #426480]] добавил логгер

--- src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
@@ -1,5 +1,7 @@
 package ru.job4j.dreamjob.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.sql2o.Sql2o;
 import ru.job4j.dreamjob.model.User;
@@ -8,6 +10,7 @@ import java.util.Optional;
 
 @Repository
 public class Sql2oUserRepository implements UserRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(Sql2oUserRepository.class.getName());
     private final Sql2o sql2o;
 
     public Sql2oUserRepository(Sql2o sql2o) {
@@ -16,6 +19,7 @@ public class Sql2oUserRepository implements UserRepository {
 
     @Override
     public Optional<User> save(User user) {
+        int generatedId = -1;
         try (var connection = sql2o.open()) {
             var sql = """
                     INSERT INTO users(email, name, password)
@@ -25,12 +29,12 @@ public class Sql2oUserRepository implements UserRepository {
                     .addParameter("email", user.getEmail())
                     .addParameter("name", user.getName())
                     .addParameter("password", user.getPassword());
-            int generatedId = query.executeUpdate().getKey(Integer.class);
+            generatedId = query.executeUpdate().getKey(Integer.class);
             user.setId(generatedId);
         } catch (Exception exception) {
-            return Optional.empty();
+            LOG.info("Регистрация зарегистрированного пользователя, Exception in log example", exception);
         }
-        return Optional.of(user);
+        return generatedId == user.getId() ? Optional.of(user) : Optional.empty();
     }
 
     @Override
